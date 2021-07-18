57c3a8a538a16610823e01e8d1b584704e8bce99
Vaishali2021 <72402488+Vaishali2021@users.noreply.github.com>
2021-07-18 23:14:16 +0530

    Code

---
+++ Week0/linear_search.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+   int i,n,x;
+   printf("Enter the size of the array");
+   scanf("%d",&n);
+   int arr[n];
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&arr[i]);
+   }
+   printf("Enter the number to be searched");
+   scanf("%d",&x);
+   for(i=0;i<n;++i)
+        if(arr[i]==x)
+            break;
+     
+    if(i<n)
+        printf("Element found at index %d",i);
+    else
+        printf("Element not found");
+  
+    return 0;
+}
+
