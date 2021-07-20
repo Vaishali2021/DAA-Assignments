e102c3366edbb8c4e5b722c03e44e8b135ddb831
Vaishali2021 <72402488+Vaishali2021@users.noreply.github.com>
2021-07-20 21:12:05 +0530

    Code

--- Week1/q1.c
+++ Week1/q1.c
@@ -6,23 +6,21 @@ int main()
     while(t--)
    { 
    int i,n,x;
-   printf("Enter the size of the array");
    scanf("%d",&n);
    int arr[n];
    for(i=0;i<n;i++)
    {
        scanf("%d",&arr[i]);
    }
-   printf("Enter the number to be searched");
    scanf("%d",&x);
    for(i=0;i<n;++i)
         if(arr[i]==x)
             break;
      
     if(i<n)
-        printf("Element found at index %d\n",i+1);
+        printf("Present %d\n",i+1);
     else
-        printf("Element not found");
+        printf("Not Present %d\n",i);
    }    
     return 0;
 }
